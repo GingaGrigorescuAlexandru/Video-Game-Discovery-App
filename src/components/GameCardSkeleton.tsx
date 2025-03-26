3371fe12bd06ef1b6afcba07ae86060d0bea78b8
ALEX13GBeb <167245668+ALEX13GBeb@users.noreply.github.com>
2025-03-26 22:58:53 +0200

    Adjust the width of the GamaCardSkeletons and refactor the theme

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, Skeleton, CardBody, SkeletonText} from '@chakra-ui/react';
 
 const GameCardSkeleton = () => {
     return (
-        <Card>
+        <Card width='350px'>
             <Skeleton height="200px"/>
             <CardBody>
                 <SkeletonText />
