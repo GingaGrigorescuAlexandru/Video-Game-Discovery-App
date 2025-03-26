417e5bb9339f9c48034f945940120e78bb951691
ALEX13GBeb <167245668+ALEX13GBeb@users.noreply.github.com>
2025-03-26 22:52:59 +0200

    Add separate return statement for errors in GameGrid.tsx

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,6 +13,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     const { data: games, error, loading } = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+    if (error) return <Text>{error}</Text>;
     return (
         <>
         <SimpleGrid columns={{ sm: 1, md: 2, lg:3, xl: 4}}
